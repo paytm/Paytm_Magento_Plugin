36105d5a311958bc12854ec9b2f2cca3ca641363
Prabhat Mishra <35480210+Prabhat1Mishra@users.noreply.github.com>
2018-03-27 15:39:36 +0530

    Updated block/info/paytm.php file

--- Magento_v2.x/Magento Plugin V2.0/app/code/One97/Paytm/Block/Info/Paytm.php
+++ Magento_v2.x/Magento Plugin V2.0/app/code/One97/Paytm/Block/Info/Paytm.php
@@ -4,5 +4,5 @@ namespace One97\Paytm\Block\Info;
 
 class Paytm extends \Magento\Payment\Block\Info
 {
-    protected $_template = 'One97_Paytm::info/paytm.phtml';
+    // protected $_template = 'One97_Paytm::info/paytm.phtml';
 }
