1bf2f0d21d362f0e42300e18020ad170232a94e8
Abhishek Awasthi <74229697+abpaytm@users.noreply.github.com>
2021-04-20 16:53:27 +0530

    Update PaytmConfigProvider.php

--- Magento_v2.x/app/code/One97/Paytm/Model/PaytmConfigProvider.php
+++ Magento_v2.x/app/code/One97/Paytm/Model/PaytmConfigProvider.php
@@ -3,22 +3,27 @@
     use Magento\Checkout\Model\ConfigProviderInterface;
     use Magento\Payment\Helper\Data as PaymentHelper;
     use Magento\Framework\UrlInterface as UrlInterface;
+    use \Magento\Framework\App\Config\ScopeConfigInterface as ScopeConfig;
 
     class PaytmConfigProvider implements ConfigProviderInterface {
         protected $methodCode = "paytm";
         protected $method;
         protected $urlBuilder;
 
-        public function __construct(PaymentHelper $paymentHelper, UrlInterface $urlBuilder) {
+        public function __construct(PaymentHelper $paymentHelper, UrlInterface $urlBuilder, ScopeConfig $scopeConfig ) {
             $this->method = $paymentHelper->getMethodInstance($this->methodCode);
             $this->urlBuilder = $urlBuilder;
+            $this->scopeConfig = $scopeConfig;
         }
 
         public function getConfig() {
             return $this->method->isAvailable() ? [
                 'payment' => [
                     'paytm' => [
-                        'redirectUrl' => $this->urlBuilder->getUrl('paytm/Standard/Redirect', ['_secure' => true])
+                        'mid' => $this->method->getMID(),
+                        'checkout_url' => $this->method->getcheckoutjsurl(),
+                       // 'order' => $this->method->getOrder(),
+                        'redirecturl' => $this->urlBuilder->getUrl('paytm/Standard/Redirect', ['_secure' => true])
                     ]
                 ]
             ] : [];
