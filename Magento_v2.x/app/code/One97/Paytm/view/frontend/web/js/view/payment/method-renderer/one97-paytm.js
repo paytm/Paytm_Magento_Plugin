be06028bbb71a50557e67d01668038cb71ed40e4
Abhishek Awasthi <74229697+abpaytm@users.noreply.github.com>
2021-04-20 16:55:20 +0530

    Update one97-paytm.js

--- Magento_v2.x/app/code/One97/Paytm/view/frontend/web/js/view/payment/method-renderer/one97-paytm.js
+++ Magento_v2.x/app/code/One97/Paytm/view/frontend/web/js/view/payment/method-renderer/one97-paytm.js
@@ -12,7 +12,37 @@ define(
         'use strict';
         return Component.extend({
             defaults: {
-                template: 'One97_Paytm/payment/one97'
+                template: 'One97_Paytm/payment/one97',
+                paytmDataFrameLoaded: false,
+                redirectUrl: ''
+            },
+            isAvailable: function() {
+                return this.paytmDataFrameLoaded;
+            },
+            getMerchantid: function() {
+                return window.checkoutConfig.payment.paytm.mid;
+            },
+            getCheckoutUrl: function() {
+                return window.checkoutConfig.payment.paytm.checkout_url;
+            },
+            getRedirecturl: function() {
+                return window.checkoutConfig.payment.paytm.redirecturl;
+            },
+            //  getordrid: function() {
+            //      alert(window.checkoutConfig.payment.paytm.order);
+            //      return window.checkoutConfig.payment.paytm.order;
+            //  },
+            initObservable: function() {
+                var self = this._super();              //Resolves UI Error on Checkout
+
+
+                if(!self.paytmDataFrameLoaded) {
+                    $.getScript(self.getCheckoutUrl(), function() {
+                        self.paytmDataFrameLoaded = true;
+                       
+                    });
+                }
+                return self;
             },
             placeOrder: function (data, event) {
                 if (event) {
@@ -38,7 +68,58 @@ define(
                 return false;
             },
             afterPlaceOrder: function () {
-                $.mage.redirect(window.checkoutConfig.payment.paytm.redirectUrl);
+                var self = this;
+                $.ajax({
+                    type: 'POST',
+                   // url: urlBuilder.build("Standard/Success"),
+                    url: window.checkoutConfig.payment.paytm.redirecturl,
+                    data: {
+                        email: 'test@gmail.com',
+                    },
+
+                    /**
+                     * Success callback
+                     * @param {Object} response
+                     */
+                    success: function (response) {
+                        if (response.txnToken) {
+                            self.renderCheckout(response);
+                           
+                        } 
+                    },
+                });
+				
+            },
+
+            renderCheckout: function(data) {
+                var config = {
+                    "root": "",
+                    "flow": "DEFAULT",
+                    "data": {
+                            "orderId": data.orderId,
+                            "token": data.txnToken,
+                            "tokenType": "TXN_TOKEN",
+                            "amount": data.amount,
+                    },
+                    "handler": {
+                        "notifyMerchant": function(eventName,data){
+                            if(eventName == 'SESSION_EXPIRED'){
+                                $('a[href="#collapse-payment-method"]').click();
+                            }
+                        } 
+                    }
+                    };
+                
+                    
+                    if(window.Paytm && window.Paytm.CheckoutJS){
+                            // initialze configuration using init method 
+                            window.Paytm.CheckoutJS.init(config).then(function onSuccess() {
+                            // after successfully update configuration invoke checkoutjs
+                            window.Paytm.CheckoutJS.invoke();
+                            }).catch(function onError(error){
+                                console.log("error => ",error);
+                            });
+                    }  
             }
         });
     }
