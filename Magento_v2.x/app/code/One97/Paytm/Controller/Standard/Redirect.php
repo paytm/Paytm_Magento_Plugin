3b851d334895a3bcf60e49c9dc00ac7ea5a0a26d
Abhishek Awasthi <74229697+abpaytm@users.noreply.github.com>
2021-04-20 16:49:40 +0530

    Update Redirect.php

--- Magento_v2.x/app/code/One97/Paytm/Controller/Standard/Redirect.php
+++ Magento_v2.x/app/code/One97/Paytm/Controller/Standard/Redirect.php
@@ -1,37 +1,84 @@
 <?php
-    namespace One97\Paytm\Controller\Standard;
 
-    class Redirect extends \One97\Paytm\Controller\Paytm {
-        
-        /* this funtion redirect to Paytm with proper form post */
-        public function execute() {
-            $promo='';
-            if(isset($_GET['promo'])){
-                if(trim($_GET['promo'])!=''){
-                    $promo=$_GET['promo'];
-                }
-            }
-            $order = $this->getOrder();
-            if ($order->getBillingAddress()) {
-                $order->setState("pending_payment")->setStatus("pending_payment");
-                $order->addStatusToHistory($order->getStatus(), "Customer was redirected to paytm.");
-                $order->save();
-                if($promo!=''){
-                    $order->paytmPromoCode=$promo;
-                }
-                $data['inputForm']=$this->_paytmModel->buildPaytmRequest($order);
-                $data['actionURL']=$this->_paytmModel->getRedirectUrl();
-                $resultPage = $this->resultPageFactory->create();
-                $resultPage->getLayout()->initMessages();
-                $resultPage->getLayout()->getBlock('paytm_standard_redirect')->setName($data);
-                return $resultPage;
-            } else {
-                $this->_cancelPayment();
-                $this->_paytmSession->restoreQuote();
-                $this->getResponse()->setRedirect(
-                    $this->getPaytmHelper()->getUrl('checkout')
-                );
-            }
+  
+    namespace One97\Paytm\Controller\Standard;
+    
+    
+    use Magento\Sales\Model\Order;
+    
+    class Redirect extends \Magento\Framework\App\Action\Action
+    {
+        protected $_pageFactory;
+        protected $_resultJsonFactory;
+        protected $_checkoutSession;
+        protected $orderRepository;
+        protected $customerSession;
+        protected $_paytmModel;
+        protected $_paytmHelper;
+    
+        public function __construct(
+            \Magento\Framework\App\Action\Context $context,
+            \Magento\Framework\Controller\Result\JsonFactory $resultJsonFactory,
+            \Magento\Framework\View\Result\PageFactory $pageFactory,
+            \Magento\Checkout\Model\Session $checkoutSession,
+            \Magento\Sales\Api\OrderRepositoryInterface $orderRepository,
+            \Magento\Customer\Model\Session $customerSession,
+            \One97\Paytm\Model\Paytm $paytmModel,
+            \One97\Paytm\Helper\Data $paytmHelper
+        )
+        {
+            $this->_checkoutSession = $checkoutSession;
+            $this->_resultJsonFactory = $resultJsonFactory;
+            $this->_pageFactory = $pageFactory;
+            $this->orderRepository = $orderRepository;
+            $this->customerSession = $customerSession;
+            $this->_paytmModel = $paytmModel;
+            $this->_paytmHelper = $paytmHelper;
+            return parent::__construct($context);
         }
+    
+        public function execute()
+        {
+    
+    
+    
+            $customerId = $this->customerSession->getCustomer()->getId();
+    
+            $result = $this->_resultJsonFactory->create();
+    
+            $order = $this->_checkoutSession->getLastRealOrder();
+            //$orderId=$order->getEntityId();
+            $order->getIncrementId();
+    
+            $this->_resources = \Magento\Framework\App\ObjectManager::getInstance()->get('Magento\Framework\App\ResourceConnection');
+            // $connection= $this->_resources->getConnection();
+            // $themeTable = $this->_resources->getTableName('z_payu_tx');
+            // $sql = "INSERT INTO ". $themeTable . 
+            //         " (orderid, date, state_pol,customer_number) 
+            //         VALUES 
+            //         ('".$order->getIncrementId()."', '".date("Y-m-d H:i:s")."','NEW','".$customerId."')";
+    
+            try{
+    
+                //$connection->query($sql);
+    
+                $orderId = $order->getIncrementId();
+    
+            }catch(\Exception $e){
+    
+                $orderId = $order->getIncrementId();
+            }
+           
+            $resultdata = $this->_paytmModel->buildPaytmRequest($order);
+            //echo json_encode(array('response'=>$resultdata));
+            return $result->setData($resultdata);
+    
+    
+        }
+    
     }
+ 
 ?>
+
+
+  
