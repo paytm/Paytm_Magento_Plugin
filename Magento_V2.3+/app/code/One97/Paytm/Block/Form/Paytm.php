3a2ea34d0b36e6cb379baa823ba5f119c4d74809
Abhishek Awasthi <74229697+abpaytm@users.noreply.github.com>
2021-09-08 13:40:07 +0530

    Update Paytm.php

--- Magento_V2.3+/app/code/One97/Paytm/Block/Form/Paytm.php
+++ Magento_V2.3+/app/code/One97/Paytm/Block/Form/Paytm.php
@@ -2,7 +2,7 @@
 	namespace One97\Paytm\Block\Form;
 
 	class Paytm extends \Magento\Payment\Block\Form {
-	    public $_template = 'One97_Paytm::form/paytm.phtml';
+	   // public $_template = 'One97_Paytm::form/paytm.phtml';
 	    public $objectPaytmCont='';
 	    public function __construct(
             \Magento\Backend\Block\Template\Context $context,
